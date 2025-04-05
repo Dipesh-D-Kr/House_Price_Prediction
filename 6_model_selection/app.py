82050c1ebd86881f31ce37cd22daaabab88d153b
Dipesh-D-Kr <dipeshkumar4838@gmail.com>
2025-04-05 14:18:13 +0530

    Update app.py

--- 6_model_selection/app.py
+++ 6_model_selection/app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pickle
+import pandas as pd
 
+with open("df.pkl","rb") as a:
+  df = pd.DataFrame(a)
 
 st.header("House Price Prediction")
 st.selectbox("Select Gender",["Male","female","Others"])
+st.selectbox("Select Sector",df["sector"].unique().to_list())
